be396686a2c52153c79281ed06cdbd33906b2668
Pestrikov Anton <pestrikov@siberteam.com>
2024-10-20 09:53:47 +0300

    Commit with time tests

--- lab1/main.c
+++
@@ -1,9 +0,0 @@
-#include "headers/monte_carlo.h"
-#include "headers/mandelbrot.h"
-#include "headers/pth_ll_rwl.h"
-
-int main(int argc, char *argv[]) {
-    mandelbrot(argc, argv);
-    monte_carlo(argc, argv);
-    readWriteLock(argc, argv);
-}
