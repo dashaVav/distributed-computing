06e51c1809abc448101bb0a4f3078ed787ccffff
Pestrikov Anton <pestrikov@siberteam.com>
2024-10-20 10:13:09 +0300

    Commit with time tests

--- lab1/main_monte_carlo.c
+++ lab1/main_monte_carlo.c
@@ -6,6 +6,8 @@
 void run_monte_carlo_with_timing(FILE *file, long nthreads, long ntrials) {
     double start, end;
     GET_TIME(start);
+    char threads_str[10], trials_str[20];
+    char *args[] = {"program", threads_str, trials_str};
     monte_carlo(3, args);
     GET_TIME(end);
     double time_taken = end - start;
